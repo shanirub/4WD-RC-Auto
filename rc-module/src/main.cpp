93b0f7a04ce013d2dac89451db17f3a9d482fb01
srub <shani.rubovitch@gmail.com>
2020-11-03 14:09:00 +0100

    mdns working for both auto and rc-module

--- rc-module/src/main.cpp
+++ rc-module/src/main.cpp
@@ -11,6 +11,7 @@ Note: ADC2 pins cannot be used when Wi-Fi is used. So, if you’re using Wi-Fi a
 #include <driver/adc.h>
 #include <WiFi.h>
 #include "esp_wifi.h"
+#include <ESPmDNS.h>
 
 #define RIGHT_X 35
 #define RIGHT_Y 32
@@ -25,15 +26,15 @@ const char *password = "lalalalala";
 
 const int port = 2345;
 
-char* wifiserver_ip;
+char *wifiserver_ip;
 
 char direction = 's';
 
-int right_x_value;    // right control: 0 up, 4095 down
+int right_x_value; // right control: 0 up, 4095 down
 int right_y_value;
 int right_sw_value;
 int left_x_value;
-int left_y_value;     // left control: 0 up, 4095 down
+int left_y_value; // left control: 0 up, 4095 down
 int left_sw_value;
 
 void setup()
@@ -69,43 +70,55 @@ void setup()
   // here we need to find the station's ip connected to this ap
   wifi_sta_list_t wifi_sta_list;
   tcpip_adapter_sta_list_t adapter_sta_list;
- 
+
   memset(&wifi_sta_list, 0, sizeof(wifi_sta_list));
   memset(&adapter_sta_list, 0, sizeof(adapter_sta_list));
- 
+
   esp_wifi_ap_get_sta_list(&wifi_sta_list);
   tcpip_adapter_get_sta_list(&wifi_sta_list, &adapter_sta_list);
- 
-  for (int i = 0; i < adapter_sta_list.num; i++) {
- 
+
+  for (int i = 0; i < adapter_sta_list.num; i++)
+  {
+
     tcpip_adapter_sta_info_t station = adapter_sta_list.sta[i];
- 
+
     Serial.print("station nr ");
     Serial.println(i);
- 
+
     Serial.print("MAC: ");
- 
-    for(int i = 0; i< 6; i++){
-      
-      Serial.printf("%02X", station.mac[i]);  
-      if(i<5)Serial.print(":");
+
+    for (int i = 0; i < 6; i++)
+    {
+
+      Serial.printf("%02X", station.mac[i]);
+      if (i < 5)
+        Serial.print(":");
     }
- 
+
     Serial.print("\nIP: ");
 
-    char* station_ip = ip4addr_ntoa(&(station.ip));
+    char *station_ip = ip4addr_ntoa(&(station.ip));
     Serial.println(station_ip);
 
     wifiserver_ip = station_ip;
   }
- 
+
+  if (!MDNS.begin("rc-module"))
+  {
+    Serial.println("Error setting up MDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+
   Serial.println("-----------");
   delay(5000);
 }
 
-
-
-void print_values() {
+void print_values()
+{
   Serial.print("Right X ");
   Serial.print(right_x_value);
   Serial.print("| Y ");
@@ -125,30 +138,36 @@ void print_values() {
 
 void loop()
 {
-
-  right_x_value = analogRead(RIGHT_X);    // right control: 0 up, 4095 down
+  
+  right_x_value = analogRead(RIGHT_X); // right control: 0 up, 4095 down
   right_y_value = analogRead(RIGHT_Y);
   right_sw_value = digitalRead(RIGHT_SW);
   left_x_value = analogRead(LEFT_X);
-  left_y_value = analogRead(LEFT_Y);      // left control: 0 up, 4095 down
+  left_y_value = analogRead(LEFT_Y); // left control: 0 up, 4095 down
   left_sw_value = digitalRead(LEFT_X);
 
   print_values();
 
-  if ((right_x_value < 500) && (left_y_value < 500)) {
+  if ((right_x_value < 500) && (left_y_value < 500))
+  {
     direction = 'f';
-  } else if ((right_x_value > 3500) && (left_y_value > 3500)) {
+  }
+  else if ((right_x_value > 3500) && (left_y_value > 3500))
+  {
     direction = 'b';
-  } else if ((left_y_value > 3500) && (right_x_value < 500)) {
+  }
+  else if ((left_y_value > 3500) && (right_x_value < 500))
+  {
     direction = 'l';
-  } else if ((left_y_value < 500) && (right_x_value > 3500)) {
+  }
+  else if ((left_y_value < 500) && (right_x_value > 3500))
+  {
     direction = 'r';
-  } else
+  }
+  else
   {
     direction = 's';
   }
-  
-
 
   WiFiClient client;
   if (!client.connect(wifiserver_ip, port))
